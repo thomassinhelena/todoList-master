07db98eb52980149c50d05eb52df2d510ebd0c21
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-09-14 11:31:48 +0200

    add button add

--- assets/js/statement.js
+++ assets/js/statement.js
@@ -1,4 +1,62 @@
 $(function() {
   let element = document.getElementById("container-loader");
   element.classList.toggle("invisible");
+
+  const btnEdit = document.getElementsByClassName("edit-tasks");
+  for (let i = 0; i < btnEdit.length; i++) {
+    btnEdit[i].addEventListener("click", openModal);
+  }
+
+  const btnRemove = document.getElementsByClassName("remove-tasks");
+  for (let i = 0; i < btnRemove.length; i++) {
+    btnRemove[i].addEventListener("click", removeTask);
+  }
+
+  document.getElementById("save").addEventListener("click", saveTask);
+
+
+  function openModal(){
+    const idTasks = this.getAttribute("data-id");
+    const labelTask = this.getAttribute("data-label");
+    const url = "/tâches/"+idTasks+"/edition";
+
+    $("#modal-edit").modal("show");
+
+    $.ajax( url)
+      .done(function(result) {
+        let titre = 'Edition du projet ' + labelTasks;
+        if(labelTasks === ""){
+          titre = "Ajouter un projet"
+        }
+        document.getElementById("modal-edit-label").text = titre;
+        document.getElementById("modal-body").innerHTML =result;
+      })
+  }
+
+  function saveTask(){
+    let element = document.getElementById("container-loader");
+    element.classList.toggle("invisible");
+    document.getElementsByName("tasks")[0].submit();
+  }
+
+  function removeTask(){
+    const idTasks = this.getAttribute("data-id");
+    const url = "/tâches/"+idTasks+"/suppression";
+
+    let element = document.getElementById("container-loader");
+    element.classList.toggle("invisible");
+
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      success: function(result) {
+        if(result){
+          document.getElementById("row-"+idTasks).remove();
+          let element = document.getElementById("container-loader");
+          element.classList.toggle("invisible");
+        }
+      }
+    })
+  }
+
 });
--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\Tasks;
-use App\Form\TasksType;
+use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Repository\TasksRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
  * @Route("/tasks")
  */
 class TasksController extends AbstractController
 {
+    private TasksRepository $projectRepository;
+    private EntityManagerInterface $em;
+    /**
+     * TasksController constructor.
+     */
+    public function __construct(TasksRepository $tasksRepository, EntityManagerInterface $em)
+    {
+        $this->tasksRepository = $tasksRepository;
+        $this->em = $em;
+    }
+    
     /**
      * @Route("/", name="tasks_index", methods={"GET"})
      */
@@ -26,27 +39,55 @@ class TasksController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="tasks_new", methods={"GET","POST"})
+     * @Route("/tâche/{idTasks}/edition",
+     *     name="tasks_edit",
+     *     options={"expose": true},
+     *     methods={"GET","POST","PUT"},
+     *     requirements={"idTasks" : "\d+"})
+     * @Entity("tasks", expr="repository.findId(idTasks)")
+     * @param Tasks $tasks
+     * @param Request $request
+     * @return Response
      */
-    public function new(Request $request): Response
-    {
-        $task = new Tasks();
-        $form = $this->createForm(TasksType::class, $task);
+    public function edit(Project $tasks,Request $request):Response{
+
+        $idTasks = $tasks->getId() === null ? 0: $tasks->getId();
+        $form = $this->createForm(ProjectType::class, $tasks,["action"=>$this->generateUrl("_tasks_edit",["idTasks"=>$idTasks])]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($task);
-            $entityManager->flush();
+            $this->em->persist($tasks);
+            $this->em->flush();
+            return $this->redirectToRoute('_tasks');
 
-            return $this->redirectToRoute('tasks_index');
+        }else{
+            return $this->render('tasks/tasks-edit.html.twig', ["form"=>$form->createView()]);
         }
 
-        return $this->render('tasks/new.html.twig', [
-            'task' => $task,
-            'form' => $form->createView(),
-        ]);
     }
+
+    // /**
+    //  * @Route("/new", name="tasks_new", methods={"GET","POST"})
+    //  */
+    // public function new(Request $request): Response
+    // {
+    //     $task = new Tasks();
+    //     $form = $this->createForm(TasksType::class, $task);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($task);
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('tasks_index');
+    //     }
+
+    //     return $this->render('tasks/new.html.twig', [
+    //         'task' => $task,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 
     /**
      * @Route("/{id}", name="tasks_show", methods={"GET"})
@@ -61,22 +102,22 @@ class TasksController extends AbstractController
     /**
      * @Route("/{id}/edit", name="tasks_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Tasks $task): Response
-    {
-        $form = $this->createForm(TasksType::class, $task);
-        $form->handleRequest($request);
+    // public function edit(Request $request, Tasks $task): Response
+    // {
+    //     $form = $this->createForm(TasksType::class, $task);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('tasks_index');
-        }
+    //         return $this->redirectToRoute('tasks_index');
+    //     }
 
-        return $this->render('tasks/edit.html.twig', [
-            'task' => $task,
-            'form' => $form->createView(),
-        ]);
-    }
+    //     return $this->render('tasks/edit.html.twig', [
+    //         'task' => $task,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 
     /**
      * @Route("/{id}", name="tasks_delete", methods={"DELETE"})
